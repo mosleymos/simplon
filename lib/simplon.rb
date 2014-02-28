218cf9dcec7092cbc825b96cb2fa48ca9dc25bb1
Mori <keitamori@gmail.com>
2014-02-28 11:44:45 +0100

    Ajout de texte simplon

--- lib/simplon.rb
+++ lib/simplon.rb
@@ -2,4 +2,13 @@ require "simplon/version"
 
 module Simplon
   # Your code goes here...
+    
+    class SimplonMain
+
+        def initialize
+            puts "Simplon les codeurs"
+        end
+
+    end
+
 end
